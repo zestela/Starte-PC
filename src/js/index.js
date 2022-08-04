d8690b7ba73e8ac5dc1d78d484f6923bb1051396
LiangYin <yzh1683572180@163.com>
2022-08-04 19:05:45 +0800

    dev0.0.13_0804
    垃圾jQuery，继续使用原生DOM。
    修复往日时间排序问题。
    修复代码逻辑。

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+const $ = document.querySelector.bind(document);
 let mainpageData;
 window.electronAPI.getMainpage()
     .then(async (resolve) => {
@@ -6,10 +7,10 @@ window.electronAPI.getMainpage()
         let picUrl = (await window.electronAPI.getcwd() + "/cache/" + mainpageData.data.id + ".png");
         document.body.style.backgroundImage = `url('${picUrl}')`;
         let time = new Date().toLocaleString().split(" ")[0];
-        $("year").text(time.split("/")[0] + " 年");
-        $("date").text(time.split("/")[1] + " 月 " + time.split("/")[2] + " 日");
-        $(".mainpage-text-title").text(mainpageData.data.title);
-        $(".mainpage-text-describe").text(mainpageData.data.describe);
+        $("year").innerText = time.split("/")[0] + " 年";
+        $("date").innerText = time.split("/")[1] + " 月 " + time.split("/")[2] + " 日";
+        $(".mainpage-text-title").innerText = mainpageData.data.title;
+        $(".mainpage-text-describe").innerText = mainpageData.data.describe;
     });
 
 
