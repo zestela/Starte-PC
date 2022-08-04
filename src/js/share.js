e3ca157c902a3286a5955295e6228cc03f00c379
LiangYin <yzh1683572180@163.com>
2022-08-04 19:05:45 +0800

    dev0.0.13_0804
    垃圾jQuery，继续使用原生DOM。
    修复往日时间排序问题。
    修复代码逻辑。

--- src/js/share.js
+++ src/js/share.js
@@ -1,12 +1,13 @@
 let shareData;
+const $ = document.querySelector.bind(document);
 window.electronAPI.getMainpage()
     .then(async (resolve) => {
         resolve = JSON.parse(resolve);
         shareData = resolve;
         let picUrl = (await window.electronAPI.getcwd() + "/cache/" + shareData.data.id + ".png");
-        $(".share-text-title").text(shareData.data.title);
-        $(".share-text-describe").text(shareData.data.describe);
-        $("#mainPicture").attr("src", picUrl);
+        $(".share-text-title").innerText = shareData.data.title;
+        $(".share-text-describe").innerText = shareData.data.describe;
+        $("#mainPicture").setAttribute("src", picUrl);
         // let time = new Date().toLocaleString().split(" ")[0];
         // document.getElementsByTagName("date")[0].innerText = time.split("/")[1] + " 月 " + time.split("/")[2] + " 日";
         await html2canvas(document.getElementById("share"), { scale: 4 })
--- src/js/wallpaper-list.js
+++ src/js/wallpaper-list.js
@@ -1,11 +1,15 @@
 let dataLst;
 let nowSelect = 0;
+const $ = document.querySelector.bind(document);
+
 window.onload = async function () {
     const data = await (await fetch("https://api.discoverse.space/mainpage/get-mainpage-history-list.php?month=08&year=2022")).json();
     dataLst = data;
-    for (let i = 0; i < Object.keys(data.data).length; i++) {
-        $(".wallpaper-list").append(`
-        <div class="wallpaper-in-list" style="background-image: url(${data.data[i].url});" id="${data.data[i].id}" onclick="select(${data.data[i].id})">
+    for (let i = Object.keys(data.data).length - 1; i >= 0; i--) {
+        $(".wallpaper-list").insertAdjacentHTML(
+        'beforeend',
+        `
+        <div class="wallpaper-in-list" style="background-image: url(${data.data[i].url});" id="wallpaper-${data.data[i].id}" onclick="select(${data.data[i].id})">
             <div>
                 <div>
                     <h1>${data.data[i].title}</h1>
@@ -17,11 +21,11 @@ window.onload = async function () {
     }
 };
 function select(id) {
-    if (id != 0) {
-        $(`#${nowSelect}`).removeClass("focus");
+    if (nowSelect != 0) {
+        $(`#wallpaper-${nowSelect}`).classList.remove("focus");
         nowSelect = id;
     }
     else
         nowSelect = id;
-    $(`#${id}`).addClass("focus");
+    $(`#wallpaper-${id}`).classList.add("focus");
 }
