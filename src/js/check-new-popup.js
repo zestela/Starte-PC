0b6ead4ee39d7cc3ace5ab55f539e1e05bb7d758
prideplayer <xubingqingsr@163.com>
2022-08-21 13:35:34 +0800

    dev_0.0.23_0821
    检查更新功能完全成功！！！检查更新功能完全成功！！！检查更新功能完全成功！！！检查更新功能完全成功！！！检查更新功能完全成功！！！

---
+++ src/js/check-new-popup.js
@@ -0,0 +1,31 @@
+jsonbanbens = jQuery.ajax({
+    url: "https://api.discoverse.space/banben.json",
+    type: "GET",
+    dataType: "json",
+    async: false,
+    success: function(data) {
+    }
+  });
+jsonbanbened=jsonbanbens.responseText;
+var banbenlist =jQuery.parseJSON(jsonbanbened);
+var obj = JSON.parse(jsonbanbened);
+var newbanbenname = obj.banben[0].name;
+jsonbanbenhaos = $.ajax({
+    url: "./banbenhao.json",
+    type: "GET",
+    dataType: "json",
+    async: false,
+    success: function(data) {
+    }
+  });
+jsonbanbenhaonow=jsonbanbenhaos.responseText;
+var banbenhow = JSON.parse(jsonbanbenhaonow);
+var seebanben = banbenhow.banbenhaothis[0].edition;
+function disappeared() {
+    document.getElementById('update-tip').setAttribute("class", "update-tip");
+}
+if (newbanbenname != seebanben) {
+    document.getElementById('update-tip').setAttribute("class", "update-tip-checked");
+    document.getElementById('banbenhao').innerHTML=newbanbenname;
+    setTimeout(disappeared, 5000);
+}
