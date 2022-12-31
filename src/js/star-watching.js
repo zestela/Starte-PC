0d3ed600026b5a970b4ab8d1b34e7764892ec579
PridePlayer <xubingqingsr@163.com>
2022-12-31 22:49:23 +0800

    0.2.8-dev.1231修复了观星第一张图片显示今日图片的bug
    0.2.8-dev.1231修复了观星第一张图片显示今日图片的bug

--- src/js/star-watching.js
+++ src/js/star-watching.js
@@ -15,31 +15,8 @@ window.onload = async function () {
     photoMonth = photoDate.getMonth() + 1;
     photoDay = photoDate.getDate();
     isfromwho = "—— " + data.data[Object.keys(data.data).length - 1].from;
-    $(".star-list").insertAdjacentHTML(
-        'beforeend',
-        `
-        <div class="star-watching-in-list" style="background-image: url(${mainpageData.data.url});">
-            <div class="texts">
-
-                <div>
-                    <h1>${data.data[Object.keys(data.data).length - 1].sentence}</h1>
-                    <h3>${isfromwho}</h3>
-                </div>
-
-
-            </div>
-            <div class="star-watching-bottom">
-                <div class="disPLAYDATE">
-                    <div class="month">${photoMonth}</div>
-                    <div class="fenge"> / </div>
-                    <div class="dayte">${photoDay}</div>
-                </div>
-                <button class="onhover special-onhover" id="share" style="padding: 3px 3px 3px 3px">
-                    <img src="./icons/share.svg">
-                </button>
-            </div>
-    </div>`);
-    for (let i = Object.keys(data.data).length - 2; i >= 0; i--) {
+    
+    for (let i = Object.keys(data.data).length - 1; i >= 0; i--) {
         var photoDate1 = new Date(dataLst.data[i].date);
         var photoMonth1 = photoDate1.getMonth() + 1;
         var photoDay1 = photoDate1.getDate();
