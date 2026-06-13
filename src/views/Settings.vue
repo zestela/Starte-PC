d67a9ddb07b22f2fb3d8dad768249a22563d7888
LiangYin233 <liangyingugugu@outlook.com>
2026-06-13 09:57:36 +0800

    fix: 强制限制 Settings 整个页面高度
    - 添加 h-full 确保容器高度为 100%
    - 配合 overflow-hidden 彻底禁用滚动
    - setting-contain 的 position: absolute + top/bottom 应该已经限制高度，但加 h-full 确保生效

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="setting-contain overflow-hidden">
+  <div class="setting-contain h-full overflow-hidden">
     <div class="setting-left">
       <div class="setting-nav" :class="{ 'bg-[#454545]': tab === 'settings' }" @click="tab = 'settings'">
         <IconSettingsNormal />
